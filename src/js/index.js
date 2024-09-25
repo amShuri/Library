dfbe18652b8137dc4db27ac1ddc10f523e8bac2a
Shuri <julian.garrocho712@gmail.com>
2024-09-25 17:49:36 -0300

    Implement buttons to update status & delete books

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,9 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.updateReadStatus = function () {
+    this.read = this.read ? false : true;
+  };
 }
 
 function getBook() {
@@ -12,7 +15,6 @@ function getBook() {
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
   const read = document.querySelector("#read").checked;
-
   return [title, author, pages, read];
 }
 
@@ -25,27 +27,53 @@ function addBookToLibrary() {
   modal.close();
 }
 
-function createBookCard() {
-  const [title, author, pages, read] = getBook();
-
+function createBookCards() {
   const books = document.querySelector("#books");
-  books.insertAdjacentHTML(
-    "beforeend",
-    `
-    <div class="book-card">
-      <h3>Title: ${title}</h3>
-      <p>Author: ${author}</p>
-      <p>Pages: ${pages}</p>
-      <p>Status: ${read ? "Read" : "Not read yet"}</p>
-    </div>
-    `
-  );
+  books.innerHTML = "";
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+    const bookId = i;
+
+    books.insertAdjacentHTML(
+      "beforeend",
+      `
+       <div class="book-card" data-book-id="${bookId}">
+         <h3>Title: ${book.title}</h3>
+         <p>Author: ${book.author}</p>
+         <p>Pages: ${book.pages}</p>
+         <p class="status">Status: ${book.read ? "Read" : "Not read"}</p>
+         <div class="actions">
+          <button class="edit" id="edit">Update Status</button>
+          <button class="delete">Delete</button>
+         </div>
+       </div>
+       `
+    );
+  }
 }
+
+document.querySelector("#books").addEventListener("click", (e) => {
+  const bookCard = e.target.closest(".book-card");
+  if (!bookCard) return;
+
+  const bookId = bookCard.dataset.bookId;
+  if (e.target.classList.contains("delete")) {
+    myLibrary.splice(bookId, 1);
+  } else if (e.target.classList.contains("edit")) {
+    const book = myLibrary[bookId];
+
+    book.updateReadStatus();
+    bookCard.querySelector(".status").textContent = `Status: ${
+      book.read ? "Read" : "Not read"
+    }`;
+  }
+});
 
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
   addBookToLibrary();
-  createBookCard();
+  createBookCards();
 
   // Reset the form so the user doesn't have to manually empty the inputs.
   e.target.reset();
